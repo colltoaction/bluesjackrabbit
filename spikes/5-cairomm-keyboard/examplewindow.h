4eeb89cf9eefcfd6105c6f85b34e7736214b6d6f
Martín Coll <martingonzalezcoll@gmail.com>
2015-10-18 01:43:14 -0300

    Simplifico el código y borro cosas que no se usaban

--- spikes/5-cairomm-keyboard/examplewindow.h
+++ spikes/5-cairomm-keyboard/examplewindow.h
@@ -10,8 +10,6 @@
 
 class ExampleWindow : public Gtk::Window {
  public:
-    ExampleWindow();
-
     explicit ExampleWindow(Surface *surface);
 
     virtual ~ExampleWindow();
@@ -22,18 +20,6 @@ class ExampleWindow : public Gtk::Window {
 
     virtual bool on_key_press_event(GdkEventKey *event);
 
-    bool entryKeyRelease(GdkEventKey *event);
-
-    bool gridKeyRelease(GdkEventKey *event);
-
-    bool windowKeyReleaseBefore(GdkEventKey *event);
-
-    bool windowKeyRelease(GdkEventKey *event);
-
-    Gtk::Grid m_container;
-    Gtk::Label m_label;
-    Gtk::Entry m_entry;
-    Gtk::CheckButton m_checkbutton_can_propagate;
     Surface *surface;
 };
 
