d7e6001644f9a2d8922ec3f8fe371d3d0e77c00a
Tomas <tomasmussi@gmail.com>
2015-10-17 23:53:54 -0300

    Agregado un ejemplo con cairo con event key listener para poder mover el circulo del ejemplo 4 por la pantalla con las flechas del teclado

---
+++ spikes/5-cairomm-keyboard/Surface.h
@@ -0,0 +1,26 @@
+#ifndef BLUESJACKRABBIT_SURFACE_H
+#define BLUESJACKRABBIT_SURFACE_H
+
+#include <gtkmm/drawingarea.h>
+
+class Surface : public Gtk::DrawingArea {
+public:
+    Surface();
+    virtual ~Surface();
+    void moverArriba();
+    void moverAbajo();
+    void moverIzq();
+    void moverDer();
+
+protected:
+    //Override default signal handler:
+    virtual bool on_draw(const Cairo::RefPtr<Cairo::Context> &cr);
+private:
+	bool primero;
+	double centro_x;
+	double centro_y;
+	double radio;
+};
+
+
+#endif //BLUESJACKRABBIT_SURFACE_H
---
+++ spikes/5-cairomm-keyboard/examplewindow.h
@@ -0,0 +1,32 @@
+#ifndef GTKMM_EVENT_PROPAGATION_H
+#define GTKMM_EVENT_PROPAGATION_H
+
+#include "Surface.h"
+#include <gtkmm.h>
+
+class ExampleWindow : public Gtk::Window
+{
+public:
+	ExampleWindow();
+	ExampleWindow(Surface* surface);
+	virtual ~ExampleWindow();
+
+private:
+	//Override default signal handler:
+	virtual bool on_key_release_event(GdkEventKey* event);
+
+	virtual bool on_key_press_event(GdkEventKey* event);
+
+	bool entryKeyRelease(GdkEventKey* event);
+	bool gridKeyRelease(GdkEventKey* event);
+	bool windowKeyReleaseBefore(GdkEventKey* event);
+	bool windowKeyRelease(GdkEventKey* event);
+
+	Gtk::Grid m_container;
+	Gtk::Label m_label;
+	Gtk::Entry m_entry;
+	Gtk::CheckButton m_checkbutton_can_propagate;
+	Surface* surface;
+};
+
+#endif //GTKMM_EVENT_PROPAGATION_H
