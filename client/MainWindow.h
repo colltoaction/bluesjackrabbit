b3ffc501e0c2564d6c122a6661967f9d5bb2d5bf
Tomas <tomasmussi@gmail.com>
2015-10-21 01:17:47 -0300

    Agrego al cliente dos archivos de glade para levantar dos frames. Uno es el frame de inicio que permite crear una partida nueva, unirse a una existente o salir. La otra es para crear una partida nueva en el sv. Se comenta el surface para hacer graficos porque hay que lograr conectar señales de clicks para intercambiar de pantallas. Cuando finalmente este implementado, la idea es que el 'Scene' que es un DrawingArea se muestre cuando comience el juego.

--- client/MainWindow.h
+++ client/MainWindow.h
@@ -1,12 +1,18 @@
 #ifndef BLUESJACKRABBIT_CLIENT_MAINWINDOW_H
 #define BLUESJACKRABBIT_CLIENT_MAINWINDOW_H
 
+#include <gtkmm.h>
 #include <gtkmm/window.h>
+#include <gtkmm/frame.h>
 
 class MainWindow : public Gtk::Window {
+ private:
+    Gtk::Frame *mainFrame;
+    Gtk::Paned *newGamePane;
  public:
     explicit MainWindow(EventBus *eventBus);
     virtual ~MainWindow();
+    void loadFrameFromGlade(std::string fileName, Gtk::Widget *mainWidget);
 
  private:
     static const unsigned int timeout_value = 1;  // smallest timeout avaiable
--- client/main.cpp
+++ client/main.cpp
@@ -4,6 +4,8 @@
 #include "Renderer.h"
 #include "Scene.h"
 
+#include <gtkmm/button.h>
+
 int main(int argc, char *argv[]) {
     Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
     EventBus eventBus;
@@ -14,7 +16,8 @@ int main(int argc, char *argv[]) {
     eventBus.SubscribeKeyPress(GDK_KEY_Left, sigc::mem_fun(circle.Transform(), &Transform::MoveLeft));
     eventBus.SubscribeKeyPress(GDK_KEY_Right, sigc::mem_fun(circle.Transform(), &Transform::MoveRight));
     MainWindow window(&eventBus);
-    window.add(scene);
+    // window.add(scene);
+
     window.show_all();
     return app->run(window);
 }
