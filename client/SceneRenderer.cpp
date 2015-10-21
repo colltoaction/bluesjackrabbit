8bd2d37a4cee8a950b597f97b137fa13816d8e41
Martín Coll <martingonzalezcoll@gmail.com>
2015-10-21 01:10:52 -0300

    Mágicamente (o sea, usando Álgebra) muevo el centro del render al centro de la pantalla.

--- client/SceneRenderer.cpp
+++ client/SceneRenderer.cpp
@@ -7,6 +7,8 @@ void SceneRenderer::Update() {
 
 // TODO(tinchou): avoid redrawing everything
 bool SceneRenderer::on_draw(const Cairo::RefPtr<Cairo::Context> &cr) {
+    Cairo::Matrix matrix = Cairo::Matrix(1, 0, 0, 1, get_width() / 2, get_height() / 2);
+    cr->transform(matrix);
     for (std::vector<GameObject>::iterator gameObject = serverProxy->GameObjects().begin();
          gameObject != serverProxy->GameObjects().end();
          ++gameObject) {
