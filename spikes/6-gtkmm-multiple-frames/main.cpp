b465a4b27b82adf8d416f6090727d313605ac884
Martín Coll <martingonzalezcoll@gmail.com>
2015-10-22 13:31:40 -0300

    Cpplint

--- spikes/6-gtkmm-multiple-frames/main.cpp
+++ spikes/6-gtkmm-multiple-frames/main.cpp
@@ -4,8 +4,7 @@
 #include <cstdlib>
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
     ExampleWindow window;
     return app->run(window);
--- spikes/6-gtkmm-multiple-frames/packbox.cpp
+++ spikes/6-gtkmm-multiple-frames/packbox.cpp
@@ -1,43 +1,23 @@
-//$Id: packbox.cc 836 2007-05-09 03:02:38Z jjongsma $ -*- c++ -*-
-
-/* gtkmm example Copyright (C) 2002 gtkmm development team
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
-
 #include "packbox.h"
 
 PackBox::PackBox(bool homogeneous, int spacing, Gtk::PackOptions options,
-        int padding)
-: Gtk::Box(Gtk::ORIENTATION_HORIZONTAL, spacing),
-  m_button1("box.pack_start("),
-  m_button2("button,"),
-  m_button3((options == Gtk::PACK_SHRINK) ? "Gtk::PACK_SHRINK" :
-            ((options == Gtk::PACK_EXPAND_PADDING) ?
-             "Gtk::PACK_EXPAND_PADDING" : "Gtk::PACK_EXPAND_WIDGET"))
-{
-  set_homogeneous(homogeneous);
+                 int padding)
+        : Gtk::Box(Gtk::ORIENTATION_HORIZONTAL, spacing),
+          m_button1("box.pack_start("),
+          m_button2("button,"),
+          m_button3((options == Gtk::PACK_SHRINK) ? "Gtk::PACK_SHRINK" :
+                    ((options == Gtk::PACK_EXPAND_PADDING) ?
+                     "Gtk::PACK_EXPAND_PADDING" : "Gtk::PACK_EXPAND_WIDGET")) {
+    set_homogeneous(homogeneous);
 
-  pack_start(m_button1, options, padding);
-  pack_start(m_button2, options, padding);
-  pack_start(m_button3, options, padding);
+    pack_start(m_button1, options, padding);
+    pack_start(m_button2, options, padding);
+    pack_start(m_button3, options, padding);
 
-  m_pbutton4 = new Gtk::Button(Glib::ustring::format(padding) + ");");
-  pack_start(*m_pbutton4, options, padding);
+    m_pbutton4 = new Gtk::Button(Glib::ustring::format(padding) + ");");
+    pack_start(*m_pbutton4, options, padding);
 }
 
-PackBox::~PackBox()
-{
-  delete m_pbutton4;
+PackBox::~PackBox() {
+    delete m_pbutton4;
 }
