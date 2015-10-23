45a08d7289432e69279f5177e18b0cad750794d4
tomasmussi@gmail.com <tomasmussi@gmail.com>
2015-10-23 14:53:17 -0300

    Va mejorando, ahora se puede bindear porque devuelvo el builder de glade para poder obtener botones y conectarlos con signals. Faltaria revisar que punteros hay que borrar porque seguramente estemos perdiendo memoria

--- client/MainWindow.h
+++ client/MainWindow.h
@@ -1,28 +1,35 @@
 #ifndef BLUESJACKRABBIT_CLIENT_MAINWINDOW_H
 #define BLUESJACKRABBIT_CLIENT_MAINWINDOW_H
 
+
+#include <gtkmm/box.h>
+#include <gtkmm/builder.h>
 #include <gtkmm/frame.h>
 #include <gtkmm/paned.h>
 #include <gtkmm/widget.h>
 #include <gtkmm/window.h>
-#include "NewGameScreen.h"
 #include "SceneRenderer.h"
 
 typedef sigc::slot<void> Clickeable;
 
-class MainWindow: public Gtk::Window {
+class MainWindow : public Gtk::Window {
  public:
-  explicit MainWindow(SceneRenderer *scene);
-  virtual ~MainWindow();
-  void new_game_click();
+    explicit MainWindow(SceneRenderer *scene);
+    virtual ~MainWindow();
+
 
  private:
-  Gtk::Box main_frame;
-  Gtk::Box initial_screen;
-  Gtk::Box new_game_screen;
+    Gtk::Box main_frame;
+    Gtk::Box initial_screen;
+    Gtk::Box new_game_screen;
 
-  SceneRenderer *scene;
-  void load_from_glade(std::string file_name, Gtk::Widget *parent, bool signal);
+    SceneRenderer *scene;
+    Glib::RefPtr<Gtk::Builder> load_from_glade(std::string file_name, Gtk::Widget *parent);
+    void init_main_game_screen();
+    void init_new_game_screen();
+
+    void new_game_click();
+    void init_click();
 };
 
 #endif  // BLUESJACKRABBIT_CLIENT_MAINWINDOW_H
