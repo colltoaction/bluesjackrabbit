a4df9623b59659bfe67c2beecf9317463338e9b7
tomas <tomasmussi@gmail.com>
2015-10-30 13:51:35 -0300

    Agrego la estructura basica que necesita el servidor. Dejo compilando la libreria engine aparte. Podriamos usarla como libreria o pegar directamente el codigo en el proyecto server, el problema es que la clase Engine usa una senial de glib para hacer el loop de iteracion

---
+++ server/Thread.h
@@ -0,0 +1,16 @@
+#ifndef BLUESJACKRABBIT_SERVER_THREAD_H
+#define BLUESJACKRABBIT_SERVER_THREAD_H
+
+class Thread {
+ public:
+    virtual ~Thread() { }
+    void Start();
+    void Join();
+ protected:
+    virtual void ThreadMain() = 0;
+ private:
+    pthread_t thread;
+    static void* StartRoutine(void* arg);
+};
+
+#endif // BLUESJACKRABBIT_SERVER_THREAD_H
--- server/main.cpp
+++ server/main.cpp
@@ -1,20 +1,8 @@
+#include <engine/Vector.h>
 #include <iostream>
-#include <stdlib.h>
-#include <sstream>
-#include <string>
 
-#include "Server.h"
-
-#define ARGS_SERVER 2
-
-int main(int argc, char *argv[]) {
-  std::string config_filename = "server.ini";
-  if (argc == ARGS_SERVER) {
-    config_filename = std::string(argv[1]);
-  }
-
-  Configuration config(config_filename);
-  Server server(config);
-  server.init();
+int main(void) {
+  Vector v(0, 1);
+  std::cout << v.y() << std::endl;
   return 0;
 }
