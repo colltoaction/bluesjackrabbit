769725fa834a9e1b8c390773cc914cc2835b06f8
tomas <tomasmussi@gmail.com>
2015-11-04 11:41:10 -0300

    Arreglo varias cosas que estaban mal, paso mis implementaciones de socket thread etc, que estaban bien en el server pero no en el client. Descomento el lint y arreglo algunas cositas que me marcaba el lint

--- client/Mutex.cpp
+++ client/Mutex.cpp
@@ -4,18 +4,18 @@
 #include <string.h>
 
 Mutex::Mutex() {
-	memset(&this->mutex, 0, sizeof(pthread_mutex_t));
-	pthread_mutex_init(&this->mutex, NULL);
+  memset(&this->mutex, 0, sizeof(pthread_mutex_t));
+  pthread_mutex_init(&this->mutex, NULL);
 }
 
 Mutex::~Mutex() {
-	pthread_mutex_destroy(&this->mutex);
+  pthread_mutex_destroy(&this->mutex);
 }
 
-void Mutex::lock(){
-	pthread_mutex_lock(&this->mutex);
+void Mutex::lock() {
+  pthread_mutex_lock(&this->mutex);
 }
 
-void Mutex::unlock(){
-	pthread_mutex_unlock(&this->mutex);
+void Mutex::unlock() {
+  pthread_mutex_unlock(&this->mutex);
 }
--- client/RemoteServerProxy.cpp
+++ client/RemoteServerProxy.cpp
@@ -22,7 +22,7 @@ void RemoteServerProxy::MoveUp() {
   Lock l(&mutex);
   engine_.apply_force(&engine_.game_objects().front(), Vector(0, -step));
   char move = UP;
-  socket->enviar(&move, 1);
+  socket->send_buffer(&move, 1);
 }
 
 // Socket write IN GAME
@@ -30,7 +30,7 @@ void RemoteServerProxy::MoveDown() {
   Lock l(&mutex);
   engine_.apply_force(&engine_.game_objects().front(), Vector(0, step));
   char move = DOWN;
-  socket->enviar(&move, 1);
+  socket->send_buffer(&move, 1);
 }
 
 
@@ -39,7 +39,7 @@ void RemoteServerProxy::MoveLeft() {
   Lock l(&mutex);
   engine_.apply_force(&engine_.game_objects().front(), Vector(-step, 0));
   char move = LEFT;
-  socket->enviar(&move, 1);
+  socket->send_buffer(&move, 1);
 }
 
 // Socket write IN GAME
@@ -47,12 +47,13 @@ void RemoteServerProxy::MoveRight() {
   Lock l(&mutex);
   engine_.apply_force(&engine_.game_objects().front(), Vector(step, 0));
   char move = RIGHT;
-  socket->enviar(&move, 1);
+  socket->send_buffer(&move, 1);
 }
 
 // Socket recibir. This should be done after start game (not in constructor)
-RemoteServerProxy::RemoteServerProxy() : socket(NULL){
-  for (std::vector<GameObject>::iterator game_object = engine_.game_objects().begin();
+RemoteServerProxy::RemoteServerProxy() : socket(NULL) {
+  renderers_.push_back(new CharacterRenderer(&engine_.game_objects().front()));
+  for (std::vector<GameObject>::iterator game_object = engine_.game_objects().begin() + 1;
        game_object != engine_.game_objects().end();
        ++game_object) {
     renderers_.push_back(new Renderer(&(*game_object)));
@@ -60,6 +61,12 @@ RemoteServerProxy::RemoteServerProxy() : socket(NULL){
 }
 
 RemoteServerProxy::~RemoteServerProxy() {
+  for (std::vector<Renderer*>::iterator game_object = renderers_.begin();
+       game_object != renderers_.end();
+       ++game_object) {
+    delete *game_object;
+  }
+  socket->close_connection();
   delete socket;
 }
 
@@ -71,29 +78,19 @@ std::vector<Renderer> &RemoteServerProxy::renderers() {
 // recibir and write.
 bool RemoteServerProxy::connect() {
   socket = new Socket("localhost", "socks", 0);
-  socket->conectar();
+  socket->connect_socket();
 
   char caa[10];
   caa[0] = 'h';
   caa[1] = 'a';
   caa[2] = 'b';
-  socket->enviar(caa, 1);
+  socket->send_buffer(caa, 1);
 
   char message_size;
-  socket->recibir(&message_size, 1);
-  std::cout << "SIZE: " << (int)message_size << "\n";
+  socket->read_buffer(&message_size, 1);
   char c;
-  if (message_size == 1) {
-    std::cout << "ME VA A MANDAR " << (int) message_size << " BYTE\n";
-  } else {
-    std::cout << "NO DETECTO EL BYTE\n";
-  }
-  socket->recibir(&c, message_size);
-  /*std::cout << "RECIBIDO: " << c << "\n";
-  bool asd = c == 'A';
-  c = 'd';
-  socket->enviar(&c, 1);*/
-  return true;
+  socket->read_buffer(&c, message_size);
+  return c == 'A';
 }
 
 // recibir and write
