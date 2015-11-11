54e11e40cbdec5f409b40a35437bb0314e178d7d
Martín Coll <martingonzalezcoll@gmail.com>
2015-11-11 16:55:29 -0300

    Bodies documentation

--- engine/RigidBody.h
+++ engine/RigidBody.h
@@ -6,6 +6,9 @@
 #include "Transform.h"
 #include "Vector.h"
 
+/**
+ * Actual implementation of the Body class. Provides rigid body physics, like gravity.
+ */
 class RigidBody : public Body {
  public:
   explicit RigidBody(Transform *transform);
