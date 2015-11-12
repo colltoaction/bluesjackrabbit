4ec951d2436aef32fd57a93f3945fbbc6e38bc13
tomas <tomasmussi@gmail.com>
2015-11-12 18:26:21 -0300

    Simplificado aun mas el main

--- client/main.cpp
+++ client/main.cpp
@@ -1,20 +1,13 @@
 #include <gtkmm/application.h>
 #include <common/Configuration.h>
 #include "MainWindow.h"
-#include "SceneRenderer.h"
-#include "RemoteServerProxy.h"
 
-#include <stdlib.h>
-#include <sstream>
 
 int main(int argc, char *argv[]) {
-  (void)argv;  // UNUSED
   std::stringstream ss("org.fiuba.bluesjackrabbit.");
-  unsigned int v1 = 10000;
+  unsigned int v1;
   ss << (rand_r(&v1) % 10000);
-  std::string app_name;
-  ss >> app_name;
-  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, app_name);
+  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, ss.str());
   MainWindow window;
   int result = app->run(window);
   return result;
