bd340f7cf9930e6a57a8128001286a79f94b1638
Diego <diego.alfonso2@gmail.com>
2015-11-17 07:42:53 -0300

    Redundant code removed

--- editor/EditorCanvas.h
+++ editor/EditorCanvas.h
@@ -9,14 +9,27 @@ public:
   virtual ~EditorCanvas();
 
 private:
+  /**
+   * Handles Canvas's on_drag_data_received signal. It's signature matches the standard slot for
+   * that signal.
+   */  
   virtual void on_drag_data_received(
       const Glib::RefPtr<Gdk::DragContext>& context, int x, int y,
       const Gtk::SelectionData& selection_data, guint info, guint time);
+  /**
+   * Handles Canvas's on_drag_motion signal. It's signature matches the standard slot for
+   * that signal.
+   */
   virtual bool on_drag_motion(const Glib::RefPtr<Gdk::DragContext>& context, int x, int y,
       guint time);
+  /**
+   * Handles Canvas's on_drag_drop signal. It's signature matches the standard slot for
+   * that signal.
+   */
   virtual bool on_drag_drop(const Glib::RefPtr<Gdk::DragContext>& context, int x, int y,
       guint time);
   Glib::RefPtr<Goocanvas::Item> create_canvas_item(double x, double y, Gtk::Widget* icon);
+  Glib::RefPtr<Goocanvas::Item> create_canvas_item(double x, double y);
 
   Gtk::ScrolledWindow*& canvas_window_;
   Glib::RefPtr<Goocanvas::Item> dnd_item_;
