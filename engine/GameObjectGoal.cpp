3bfb5ccd31ee96eaa15672823b313d8fe655d850
tomasmussi <tomasmussi@gmail.com>
2015-11-28 17:38:09 -0300

    Pense que el finalize era de terminar el nivel, no la llamada cuando se hace shutdown del server.

--- engine/GameObjectGoal.cpp
+++ engine/GameObjectGoal.cpp
@@ -6,12 +6,9 @@ GameObjectGoal::GameObjectGoal(Body *body, Collider *collider, WinnerNotifier wi
   : GameObject(body, collider)
   , has_winner_(false)
   , winner_notifier_(winner_notifier) {
-  std::cout << "Construyendo goal\n";
 }
 
 GameObjectGoal::~GameObjectGoal() {
-  std::cout << "Destruyendo goal\n";
-  std::cout << "Este goal " << (has_winner_ ? "tenia" : "NO tenia") << " ganador\n";
 }
 
 char GameObjectGoal::game_object_type() {
@@ -20,7 +17,6 @@ char GameObjectGoal::game_object_type() {
 
 void GameObjectGoal::impact(GameObject *other) {
   if (other->game_object_type() == 'p' && !has_winner_) {
-    std::cout << "Un jugador llego a la meta\n";
     has_winner_ = true;  // This is set because sometimes we call impact twice per engine step
     winner_notifier_(dynamic_cast<GameObjectPlayer *>(other));
   }
