3e0c3b8e66c0354cd427784eb19e998bd2a5ce0c
Diego <diego.alfonso2@gmail.com>
2015-12-14 03:11:08 -0300

    Agregada lectura del nivel al EditorController

--- editor/EditorController.cpp
+++ editor/EditorController.cpp
@@ -5,32 +5,43 @@
 #include "LevelObjectType.h"
 #include "LevelWriter.h"
 #include "EditorController.h"
+#include "Map.h"
 
 #include <iostream>
 
 EditorController::EditorController()
-  : level_(new Level("Mapa de ejemplo", 1000, 1000 , 2))
-  , unsaved_changes_(false) {
+    : map_(NULL), level_(new Level("Mapa de ejemplo", 1000, 1000 , 2)),
+    level_file_name_("level1.xml"), unsaved_changes_(false) {
 }
 
 void EditorController::start_new_level() {
   if (unsaved_changes_) {
   // TODO(Diego): mostrar cuadro de dialogo - guardar cambios?
   }
+    // TODO(Diego): pasar a opcion Open
     std::cout << "Archivo -> Nuevo" << std::endl;
+    if (level_) {
+      delete level_;
+    }
+    LevelLoader loader;
+    level_ = loader.load_level(level_file_name_);
 }
 
 void EditorController::start_new_level(const Glib::VariantBase& /* parameter */) {
   start_new_level();
 }
 
-void EditorController::save_file() {
+void EditorController::save_file(const std::string& file_name) {
   LevelWriter writer(*level_);
+<<<<<<< Updated upstream
   writer.write("level1.xml");
+=======
+  writer.write(file_name);
+>>>>>>> Stashed changes
 }
 
 void EditorController::save_file(const Glib::VariantBase& /* parameter */) {
-  save_file();
+  save_file(level_file_name_);
 }
 
 void EditorController::register_object(LevelObject* object) {
