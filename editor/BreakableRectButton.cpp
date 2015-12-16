417dd149a95ffe6575b2f7a958a87d2d61f407d9
Diego <diego.alfonso2@gmail.com>
2015-12-16 14:29:12 -0300

    Solucionado problema de compilacion en versiones de gtkmm > 3.10.1

--- editor/BreakableRectButton.cpp
+++ editor/BreakableRectButton.cpp
@@ -15,14 +15,15 @@ BreakableRectButton::RectIconWidget::RectIconWidget(int width, int height) : wid
 bool BreakableRectButton::RectIconWidget::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
   cr->save();
 
-  Gdk::RGBA backgrnd_color = get_style_context()->get_background_color();
-  cr->set_source_rgba(backgrnd_color.get_red(), backgrnd_color.get_green(),
-      backgrnd_color.get_blue(), backgrnd_color.get_alpha());
-  cr->paint();
-
-  cr->set_source_rgba(1.0, 0.0, 0.0, 1.0);
+  // Gdk::RGBA backgrnd_color = get_style_context()->get_background_color();
+  // cr->set_source_rgba(backgrnd_color.get_red(), backgrnd_color.get_green(),
+  //     backgrnd_color.get_blue(), backgrnd_color.get_alpha());
+  // cr->paint();
   int real_width = get_allocated_width();
   int real_height = get_allocated_height();
+  get_style_context()->render_background(cr, 0, 0, real_width, real_height);
+
+  cr->set_source_rgba(1.0, 0.0, 0.0, 1.0);
   double x = (width_ > real_width ? 0 : real_width - width_) / 2.0;
   double y = (height_ > real_height ? 0 : real_height - height_) / 2.0;
   cr->rectangle(x, y, width_, height_ / 2.0);
